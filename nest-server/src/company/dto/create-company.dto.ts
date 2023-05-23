6cf308db50879f4e0dc8a3d2f4bbc7f130bf7f65
gggg2614 <839857519@qq.com>
2023-05-23 17:54:09 +0800

    新增公司岗位字段修改类型

--- nest-server/src/company/dto/create-company.dto.ts
+++ nest-server/src/company/dto/create-company.dto.ts
@@ -1,9 +1,10 @@
 export class CreateCompanyDto {
-    readonly comname: string
-    readonly comaria: Array<string>
-    readonly comsalary: string
-    readonly comjob: string
-    readonly industry: Array<string>
-    readonly detail:string
-    readonly comphone:number
+  readonly comname: string;
+  readonly comaria: Array<string>;
+  readonly comsalary: string;
+  readonly comjob: Array<string>;
+  readonly industry: Array<string>;
+  readonly detail: string;
+  readonly comphone: number;
+  readonly likes: Array<string>;
 }
--- nest-server/src/company/entities/company.entity.ts
+++ nest-server/src/company/entities/company.entity.ts
@@ -1,24 +1,26 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument } from "mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
 
-export type CompanyDocument = HydratedDocument<Company>
+export type CompanyDocument = HydratedDocument<Company>;
 
 @Schema()
 export class Company {
-    @Prop()
-    comname: string
-    @Prop()
-    comaria: Array<string>
-    @Prop()
-    comsalary: string
-    @Prop()
-    comjob: string
-    @Prop()
-    industry: Array<string>
-    @Prop()
-    detail: string
-    @Prop()
-    phone: number
+  @Prop()
+  comname: string;
+  @Prop()
+  comaria: Array<string>;
+  @Prop()
+  comsalary: string;
+  @Prop()
+  comjob: Array<string>;
+  @Prop()
+  industry: Array<string>;
+  @Prop()
+  detail: string;
+  @Prop()
+  phone: number;
+  @Prop()
+  like: Array<string>;
 }
 
 export const CompanySchema = SchemaFactory.createForClass(Company);
